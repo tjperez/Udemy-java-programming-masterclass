39687c56f3f0bdd46534fee682e32a6a8d2af197
tjlinares <tjlinares@github.com>
2019-04-22 22:11:35 -0400

    data types

---
+++ CharAndBoolean/src/com/tjlinares/Main.java
@@ -0,0 +1,21 @@
+package com.tjlinares;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        //Data Type: Char & Boolean
+
+        // width of 16 (2 bytes)
+        char myChar = '\u00a9';
+        System.out.println(myChar);
+
+        //only two values: true or false
+        boolean myBoolean = true;
+
+        //Challenge
+
+        char newChar = '\u00AE';
+        System.out.println(newChar);
+    }
+}
